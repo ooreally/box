12685798712e80a869eae1e176555d7810d96580
ooreally <anu1512sri@gmail.com>
2021-06-11 16:56:55 +0530

    ADD: code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+
+import styled from 'styled-components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Main>
+      <Square> </Square>
+        <Square2> </Square2>
+        <Square3> </Square3>
+        <Square4> </Square4>
+      </Main>
+        
+    </Container>
   );
 }
 
 export default App;
+
+const Container = styled.main`
+
+display:block;
+transform: translate(-10%, -50%);
+position: absolute;
+left: 50%;
+top: 50%;
+`
+const Main = styled.div`
+
+display: flex;
+align-items: center;
+min-height: 100vh;
+`
+
+const Square = styled.div`
+
+  height: 400px;
+  width: 400px;
+  border: 2px solid black;
+`
+
+const Square2 = styled(Square)`
+
+  transform: translate(-105%,-5%);
+`
+const Square3= styled(Square)`
+
+  transform: translate(-210%,-10%);
+`
+const Square4= styled(Square)`
+
+  transform: translate(-315%,-15%);
+`
